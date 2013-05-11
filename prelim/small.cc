f1a348c4d9277707c5cd159fbca40f3a444687e6
Andy Holt <ah635@cam.ac.uk>
2013-05-11 22:57:39 +0100

    return small.cc to original state

--- prelim/small.cc
+++ prelim/small.cc
@@ -4,8 +4,6 @@
 #include <cctype>
 using namespace std;
 
-void skipspaces (ifstream *infp, char &curch, bool &eofile);
-
 int main (int argc, char **argv)
 {
   ifstream inf;                // input file
@@ -23,40 +21,13 @@ int main (int argc, char **argv)
     exit(1);
   }
 
-  //  eofile = (inf.get(ch) == 0);  // get character
+  eofile = (inf.get(ch) == 0);  // get character
 
-  /*  while (!eofile) {
+  while (!eofile) {
     if (isspace(ch)) cout << '.';
     else cout << ch;
     eofile = (inf.get(ch) == 0); // get next character
   }
-  */
-
-  while (!eofile) {
-    //    cout << "in while loop" << endl;
-    //    cout << "ch = " << ch << endl;
-    inf.get(ch);
-    skipspaces(&inf, ch, eofile);
-    cout << ch;
-    //    cout << "eofile = " << eofile << endl;
-  }
 
   inf.close();
 }
-
-void skipspaces (ifstream *infp, char &curch, bool &eofile)
-{
-  //  cout << "in skipspaces" << endl;
-  while (isspace(curch) && eofile == 0) {
-    //cout << "in skipspaces while" << endl;
-    //    infp->get(curch);
-    eofile = (infp->get(curch) == 0);
-    if (curch == 0) {
-      eofile = 1;
-      return;
-    }
-  }
-  //  cout << "returning from skipspaces" << endl;
-  return;
-}
-    
