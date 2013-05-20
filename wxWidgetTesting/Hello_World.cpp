a9f91e4915a9f6e05dd83ef64d762df001c264b9
Andy Holt <ah635@cam.ac.uk>
2013-05-20 10:24:16 +0100

    updated file compiles

--- wxWidgetTesting/Hello_World.cpp
+++ wxWidgetTesting/Hello_World.cpp
@@ -1,88 +1,91 @@
+// Compile with
+//g++ `wx-config --cxxflags` Hello_World.cc  -o Hello_World `wx-config --libs --gl_libs`    -lglut -lGL -lGLU
+
 // wxWidgets "Hello World" program.
 
-// for compilers that support precompilation, include "wx/wx.h"
-//#include <wx/wxprec.h>
 
-#ifndef WX_PRECOMP
 #include <wx/wx.h>
-#endif
 
-// added this line
-#define wxDECLARE_EVENT_TABLE ;
+
+//class wxApp;
 
 class MyApp: public wxApp
 {
 public:
-  virtual bool OnInit();
+   virtual bool OnInit();
 };
 
 class MyFrame: public wxFrame
 {
 public:
-  MyFrame (const wxString& title, const wxPoint& pos, const wxSize& size);
+   MyFrame (const wxString& title, const wxPoint& pos, const wxSize& size);
 
-private:
-  void OnHello(wxCommandEvent& event);
-  void OnExit(wxCommandEvent& event);
-  void OnAbout(wxCommandEvent& event);
-
-  wxDECLARE_EVENT_TABLE()
+  //private:
+   void OnHello(wxCommandEvent& event);
+   void OnExit(wxCommandEvent& event);
+   void OnAbout(wxCommandEvent& event);
+   DECLARE_EVENT_TABLE();
 };
 
 enum
 {
-  ID_Hello = 1
+   ID_Hello = 1
 };
 
-wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-  EVT_MENU(ID_Hello,   MyFrame::OnHello)
-  EVT_MENU(wxID_EXIT,  MyFrame::OnExit)
-  EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
-wxEND_EVENT_TABLE()
 
-wxIMPLEMENT_APP(MyApp)
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+   EVT_MENU(ID_Hello,   MyFrame::OnHello)
+   EVT_MENU(wxID_EXIT,  MyFrame::OnExit)
+   EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+END_EVENT_TABLE()
+
+
+IMPLEMENT_APP(MyApp)
 
 bool MyApp::OnInit()
 {
-  MyFrame *frame = new MyFrame ( "Hello World", wxPoint(50, 50), wxSize(450, 340));
-  frame->Show(true);
-  return true;
+   MyFrame *frame = new MyFrame ( wxT("Hello World"), wxPoint(50, 50),
+wxSize(450, 340));
+   frame->Show(true);
+   return true;
 }
 
-MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-  : wxFrame(NULL, wxID_ANY, title, pos, size)
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const
+wxSize& size)
+   : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-  wxMenu *menuFile = new wxMenu;
-  menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-		   "Help string shown in status bar for this menu item");
-  menuFile->AppendSeparator();
-  menuFile->Append(wxID_EXIT);
+   wxMenu *menuFile = new wxMenu;
+  
+   menuFile->Append(ID_Hello, wxT("&Hello...\tCtrl-H"),
+		    wxT("Help string shown in status bar for this menu item"));
+   menuFile->AppendSeparator();
+   menuFile->Append(wxID_EXIT);
 
-  wxMenu *menuHelp = new wxMenu;
-  menuHelp->Append(wxID_ABOUT);
+   wxMenu *menuHelp = new wxMenu;
+   menuHelp->Append(wxID_ABOUT);
 
-  wxMenuBar *menuBar = new wxMenuBar;
-  menuBar->Append(menuFile, "&File");
-  menuBar->Append(menuHelp, "&Help");
+   wxMenuBar *menuBar = new wxMenuBar;
+		    menuBar->Append(menuFile, wxT("&File"));
+		    menuBar->Append(menuHelp, wxT("&Help"));
 
-  SetMenuBar(menuBar);
+   SetMenuBar(menuBar);
 
-  CreateStatusBar();
-  SetStatusText("Welcome to wxWidgets!");
+   CreateStatusBar();
+		    SetStatusText(wxT("Welcome to wxWidgets!"));
 }
 
 void MyFrame::OnExit(wxCommandEvent& event)
 {
-  Close(true);
+   Close(true);
 }
 
 void MyFrame::OnAbout(wxCommandEvent& event)
 {
-  wxMessageBox("This is a wxWidgets' Hello World sample",
-	       "About Hello World", wxOK | wxICON_INFORMATION);
+  wxMessageBox(wxT("This is a wxWidgets' Hello World sample"),
+	       wxT("About Hello World"), wxOK | wxICON_INFORMATION);
 }
 
 void MyFrame::OnHello(wxCommandEvent& event)
 {
-  wxLogMessage("Hello world from wxWidgets!");
+  wxLogMessage(wxT("Hello world from wxWidgets!"));
 }
