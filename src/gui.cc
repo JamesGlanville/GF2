c7de596cc2615ca6d36ac8a17811290c57e17b0e
Andy Holt <andrew.holt@hotmail.co.uk>
2013-05-29 21:29:00 +0100

    devtype confusing me

--- src/gui.cc
+++ src/gui.cc
@@ -240,7 +240,7 @@ MyFrame::MyFrame(wxWindow *parent,
   for (int i = 0; i < nmz->tablelength(); i++)
     {
       devname = nmz->getname(i);
-      cout << devname " is a " << dmz->devkind(nmz->lookup(devname)) << endl;
+      cout << devname << " is a " << dmz->devkind(nmz->lookup(devname)) << endl;
       //cout << devname << endl;
     }
 
