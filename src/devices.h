a63d29810404078748387c5b0d2a7a07bec00175
James Glanville <james.n.glanville@gmail.com>
2013-05-31 12:26:27 +0100

    first task, dtypes, might work but untested

--- src/devices.h
+++ src/devices.h
@@ -27,7 +27,7 @@ class devices{
   void execswitch (devlink d);
   void execgate (devlink d, asignal x, asignal y);
   void execxorgate(devlink d);
-  void execdtype (devlink d);
+  void execdtype (devlink d, int machinecycle);
   
   void execsigen(devlink d);
   void makesiggen (name did, vector <bool> data, bool& ok);
